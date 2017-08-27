054c2187863a1247e1ac3d7f1faa3c1f35f24e95
milagrosMndoza <milagrosmndoza@gmail.com>
2017-08-27 12:03:22 -0500

    validando mi formulario y colocando la line a con la estrela

--- js/app.js
+++ js/app.js
@@ -65,5 +65,35 @@ function cerrar() {
  }
  window.addEventListener("scroll",scroll);
 
+/*Para validar el formulario*/
+var boton = document.getElementsByClassName("boton")[0];
+boton.addEventListener("click", function(e){
+ 	e.preventDefault();
+ 	
+ 	var nombre = document.getElementById("name").value;
+ 	var email = document.getElementById("email").value;
+ 	var phone = document.getElementById("phone").value;
+ 	var message = document.getElementById("message").value;
+
+ 	if(nombre == ""){
+ 		document.getElementById("name").value = "· Ingrese su Nombre";
+ 	}	
+ 	if(email ==""){
+		document.getElementById("email").value = "· Ingrese su Email";
+ 	}
+ 	if(phone ==""){
+		document.getElementById("phone").value = "· Ingrese su Teléfono";
+ 	}
+ 	if(message == ""){
+		document.getElementById("message").value = "· Ingrese su Mensaje";
+ 	}
+ 	if(nombre != "" && email != "" && phone != "" && message != ""){
+ 		document.getElementById("name").value ="";
+ 		document.getElementById("email").value ="";
+ 		document.getElementById("phone").value ="";
+ 		document.getElementById("message").value ="";	
+ 	}		
+
+ })
 
 
